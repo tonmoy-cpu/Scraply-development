7a3fd893c1b0c389a0032537427bf8ad28b6bd2f
tommy-tester0124 <myallbackups0214@gmail.com>
2025-08-22 02:01:37 +0530

    Fix layout.tsx syntax errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,46 +31,45 @@ export default function RootLayout({
           type="image/png"
           sizes="32x32"
         />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-5QLTMJKRNP"
+        ></Script>
+        <Script
+          id="google-analytics"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-5QLTMJKRNP');
+              `,
+          }}
+        />
+        <Script
+          id="tawk_chatbot"
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+                var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+                (function(){
+                  var s1=document.createElement("script");
+                  var s0=document.getElementsByTagName("script")[0];
+                  s1.async=true;
+                  s1.src='https://embed.tawk.to/67e30e7b3f1081190ac0438f/1in7f158t';
+                  s1.charset='UTF-8';
+                  s1.setAttribute('crossorigin','*');
+                  s0.parentNode.insertBefore(s1,s0);
+                })();
+              `,
+          }}
+        />
       </head>
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-5QLTMJKRNP"
-      ></Script>
-      <Script
-        id="google-analytics"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-5QLTMJKRNP');
-            `,
-        }}
-      />{" "}
-      <Script
-      id="tawk_chatbot"
-        type="text/javascript"
-        dangerouslySetInnerHTML={{
-          __html: `
-              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-              (function(){
-                var s1=document.createElement("script");
-                var s0=document.getElementsByTagName("script")[0];
-                s1.async=true;
-                s1.src='https://embed.tawk.to/67e30e7b3f1081190ac0438f/1in7f158t';
-                s1.charset='UTF-8';
-                s1.setAttribute('crossorigin','*');
-                s0.parentNode.insertBefore(s1,s0);
-              })();
-            `,
-        }}
-      />
-      <body className={poppins.className}>
       <body className={poppins.className} suppressHydrationWarning={true}>
-      <NextTopLoader color="#28af60" showSpinner={false}/>
-      <Navbar/>
-        {children}
-        <Footer/>
+        <NextTopLoader color="#28af60" showSpinner={false}/>
+        <Navbar/>
+          {children}
+          <Footer/>
       </body>
     </html>
   );
